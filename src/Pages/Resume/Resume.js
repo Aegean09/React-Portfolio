42afeb04b620c4b2e11873b752536003a24df493
EgeDurmaz <ege_durmaz09@hotmail.com>
2022-09-14 19:45:48 +0300

    P-03 Info Card implemented

---
+++ src/Pages/Resume/Resume.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Resume.css";
+
+class Resume extends Component {
+  render() {
+    return (
+      <div className="resume">
+        DENEYİM BURAYA
+      </div>
+    );
+  }
+}
+
+export default Resume;
---
+++ src/components/infoCard/infoCard.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import photo from "../../img/Ege.jpg";
+import "./infoCard.css";
+
+class infoCard extends Component {
+  render() {
+    return (
+      <div className="card">
+        <img className="ege" src={photo} alt="EGE DURMAZ"></img>
+        <div className="grid">
+          <h1 className="card-name">Ege Durmaz</h1>
+          <div className="border"></div>
+          <h2 className="card-title">Junior Frontend Developer</h2>
+        </div>
+        <div class="card-footer">
+          <div class="card-footer-items">
+            <a
+              className="insta"
+              href="https://www.instagram.com/ege_durmz/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+            <a
+              className="medium"
+              href="https://medium.com/@ege_durmaz09"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-medium"></i>
+            </a>
+            <a
+              className="linked"
+              href="https://www.linkedin.com/in/ege-durmz"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a
+              className="git"
+              href="https://github.com/Aegean09"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default infoCard;

1438bd94f197da90c158a0814702419ad10918d7
EgeDurmaz <ege_durmaz09@hotmail.com>
2022-09-18 21:15:56 +0300

    P-09 Improved Card Component

--- src/Pages/Proje/Proje.js
+++ src/Pages/Proje/Proje.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Card from "../../components/Card/Card";
 import "./Proje.css";
-
 import { projectItems } from "./ProjeItems";
+
 class Proje extends Component {
   render() {
     return (
       <div className="proje">
         <h1 className="section-title">Projects </h1>
-        <Card prop={projectItems} />
+        <Card items={projectItems} />
       </div>
     );
   }

62f56f9a1bbf410800ff938597a1db7772081167
EgeDurmaz <ege_durmaz09@hotmail.com>
2022-09-22 22:51:00 +0300

    P-11 Photos are Added

--- src/Pages/Proje/ProjeItems.js
+++ src/Pages/Proje/ProjeItems.js
@@ -1,3 +1,10 @@
+import tyLogo from '../../img/ty.png';
+import pfrLogo from '../../img/Ege.jpg';
+import tttLogo from '../../img/ttt.png';
+import pfLogo from '../../img/Ege.jpg';
+import jaeLogo from '../../img/JAE.png';
+import mgcLogo from '../../img/DL.png';
+
 export const projectItems = [
   {
     class: "ty-project",
@@ -7,7 +14,7 @@ export const projectItems = [
     projectName: "Trendyol Form",
     technologies: "React • TypeScript • SCSS",
     githubLink: "",
-    logo: "",
+    logo: tyLogo,
     description:"Google Form like project that takes dynamic input and validations with over 10 different components developed.",
   },
   {
@@ -18,7 +25,7 @@ export const projectItems = [
     projectName: "Portfolio (With React)",
     technologies: "React • JavaScript • CSS",
     githubLink: "https://github.com/Aegean09/React-Portfolio",
-    logo: "",
+    logo: pfrLogo,
     description:"My own portfolio page. Practiced on Firebase",
   },
   {
@@ -29,7 +36,7 @@ export const projectItems = [
     projectName: "Tic-Tac-Toe",
     technologies: "Angular • TypeScript • SCSS",
     githubLink: "https://github.com/Aegean09/Angular-Tic-Tac-Toe",
-    logo: "",
+    logo: tttLogo,
     description: "Thesis project #2. Tic‑Tac‑Toe web‑page made with Angular in order to experience different frameworks. Experienced TypeScript language.",
 
   },
@@ -41,7 +48,7 @@ export const projectItems = [
     projectName: "Old Portfolio",
     technologies: "Html • Javascript • CSS • Bootstrap",
     githubLink: "https://github.com/Aegean09/Ege-Portfolio",
-    logo: "",
+    logo: pfLogo,
     description: "Current portfolio website made with traditional html/css ways.Took reference from a random web page layout found on Wix.com to challenge HTML/CSS skills.",
   },
   {
@@ -52,7 +59,7 @@ export const projectItems = [
     projectName: "JAE",
     technologies: "Html • Javascript • CSS • Bootstrap",
     githubLink: "https://github.com/leehuhlee/Web-Design",
-    logo: "",
+    logo: jaeLogo,
     description: "An imaginary restaurant web page that me and 3 other students from different nations made in Erasmus. Which I lead the project.",
   },
   {
@@ -63,7 +70,7 @@ export const projectItems = [
     projectName: "Music Genre Classification",
     technologies: "Python • Deep/Machine Learning",
     githubLink: "https://github.com/Aegean09/Music-Genre-Classification",
-    logo: "",
+    logo: mgcLogo,
     description: "Thesis project #1. Classifiying music genres using Deep Learning algorithms, Feature Engineering methods and comparing performances of algorithms.",
   },
 ];
